44b60209db5d978ea7c459bd5070505530d13d0d
NTOU <fus22803281@gmail.com>
2020-10-05 17:42:33 +0800

    Initial Commit

---
+++ 00757112_chumihua/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757112_chumihua
+//
+//  Created by User17 on 2020/10/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757112_chumihua/_0757112_chumihuaApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0757112_chumihuaApp.swift
+//  00757112_chumihua
+//
+//  Created by User17 on 2020/10/5.
+//
+
+import SwiftUI
+
+@main
+struct _0757112_chumihuaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
